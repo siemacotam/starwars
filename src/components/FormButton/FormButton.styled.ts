f565cbff4cb3199faaab60cbf217c47ac4688546
Grzegorz Celek <g.celek@gmail.com>
2022-12-02 22:22:07 +0100

    remove border

--- src/components/FormButton/FormButton.styled.ts
+++ src/components/FormButton/FormButton.styled.ts
@@ -7,6 +7,7 @@ export const FormButton = styled.button<ButtonProps>`
   color: ${({ theme }) => theme.colors.white};
   box-shadow: 10px 4px 4px rgba(0, 0, 0, 0.75);
   border-radius: 20px;
+  border: none;
   font-family: ${({ theme }) => theme.fonts.lato};
   font-size: 30px;
   max-width: 236px;
